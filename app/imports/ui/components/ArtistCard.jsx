80097be214ce3768f7177fe496bd2a340c5cef59
Caleb <100178343+CalebMueller-UH@users.noreply.github.com>
2023-04-07 18:10:08 -1000

    Finished basic layout of browse artists page in conjunction with artist card

--- app/imports/ui/components/ArtistCard.jsx
+++ app/imports/ui/components/ArtistCard.jsx
@@ -1,23 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image } from 'react-bootstrap';
+import './ArtistCardStyle.css';
+import { Card, Image, ListGroup } from 'react-bootstrap';
+
+// import { List } from 'react-bootstrap-icons';
+
+// Maximum Length of Genres that should be displayed
+const MAX_CARD_GENRES_LEN = 25;
+
+// Maximum Length of Instruments that should be displayed
+const MAX_CARD_INSTRUMENTS_LEN = 25;
+
+// Maximum Length of Bio that should be displayed
+const MAX_CARD_BIO_LEN = 135;
+
+// A function used to truncate card data to a length specified by maxlen
+const truncateTo = (data, maxlen) => {
+  if (data.length <= maxlen) {
+    return data;
+  }
+  const truncatedData = data.slice(0, maxlen);
+  const lastWord = truncatedData.lastIndexOf(' ');
+  const truncatedDataWord = truncatedData.slice(0, lastWord);
+  return `${truncatedDataWord}...`;
+};
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const ArtistCard = ({ artistEntry }) => (
   <Card className="h-100">
-
     <Card.Header>
-      <Card.Title>{artistEntry.firstName} {artistEntry.lastName}</Card.Title>
-      <Image src={artistEntry.image} width={75} />
+      <div className="d-flex justify-content-center">
+        <Card.Title>{artistEntry.firstName} {artistEntry.lastName}</Card.Title>
+      </div>
+      <div className="d-flex justify-content-center">
+        <Image src={artistEntry.image} height={100} className="image-shadow" />
+      </div>
     </Card.Header>
 
-    <Card.Body>
-      <Card.Text>
-        Genre: {artistEntry.genres}
-        Instruments: {artistEntry.instruments}, {artistEntry.skillLevel}
-        Bio: {artistEntry.bio}
-      </Card.Text>
-    </Card.Body>
+    <ListGroup variant="flush">
+
+      <ListGroup.Item className="d-flex justify-content-between align-items-center genres">
+        <span className="label fw-bold d-flex justify-content-start">Genre(s): </span>
+        <span className="content">{truncateTo(artistEntry.genres.join(', '), MAX_CARD_GENRES_LEN)}</span>
+      </ListGroup.Item>
+
+      <ListGroup.Item className="d-flex justify-content-between align-items-center instruments">
+        <span className="label fw-bold d-flex justify-content-start">Instrument(s): </span>
+        <span className="content">{truncateTo(artistEntry.instruments.join(', '), MAX_CARD_INSTRUMENTS_LEN)}</span>
+      </ListGroup.Item>
+
+      <ListGroup.Item className="d-flex justify-content-between align-items-center skillLevel">
+        <span className="label fw-bold d-flex justify-content-start">Skill Level: </span>
+        <span className="content">{artistEntry.skillLevel}</span>
+      </ListGroup.Item>
+
+      <ListGroup.Item className="d-flex justify-content-between align-items-start bio">
+        <div className="label fw-bold d-flex justify-content-start">Bio:</div>
+        <br />
+        {truncateTo(artistEntry.bio, MAX_CARD_BIO_LEN)}
+      </ListGroup.Item>
+
+    </ListGroup>
 
   </Card>
 );
--- app/imports/ui/pages/BrowseArtists.jsx
+++ app/imports/ui/pages/BrowseArtists.jsx
@@ -1,78 +1,110 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
+import './BrowseArtistsStyle.css';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
-import Contact from '../components/ArtistCard';
+import ArtistCard from '../components/ArtistCard';
+
+const dummyArtistData = [{
+  firstName: 'Albert', lastName: 'Haynes', email: 'albert.h@foo.com',
+  image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHw%3D&w=1000&q=80',
+  instruments: ['Guitar'], skillLevel: 'Intermediate',
+  genres: ['Blues', 'Psychedellic Rock'],
+  influences: ['Hendrix', 'Vaughn', 'B.B. King', 'The Doors', 'Grateful Dead'],
+  bio: 'I have been practicing guitar for a little over a year now.  I love playing gnarly riffs over some heavy bass and looking forward to the jam sessions to come!',
+},
+{
+  firstName: 'Samantha', lastName: 'Lee', email: 'samantha.l@foo.com',
+  image: 'https://images.unsplash.com/photo-1564564295391-7f24f26f568b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGt1bHxlbnwwfHwwfHw%3D&w=1000&q=80',
+  instruments: ['Bass Guitar'], skillLevel: 'Advanced',
+  genres: ['Funk', 'Jazz', 'Soul'],
+  influences: ['Victor Wooten', 'Marcus Miller', 'Thundercat', 'Earth, Wind & Fire', 'Stevie Wonder'],
+  bio: 'I have been playing bass guitar for 10 years now. I love the rhythm and groove that comes with funk, jazz, and soul music. Looking for fellow musicians who share the same passion and vibe!',
+},
+{
+  firstName: 'Alex', lastName: 'Nguyen', email: 'alex.n@foo.com',
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQayQiD1x_FnLWOHbMia0mXHg6dyd91gcgFPztytUpnPg&usqp=CAU&ec=48600112',
+  instruments: ['Drums', 'Piano'], skillLevel: 'Intermediate',
+  genres: ['Rock', 'Pop', 'Indie'],
+  influences: ['The Beatles', 'Queen', 'Radiohead', 'Coldplay', 'Tame Impala'],
+  bio: 'I started playing drums when I was 12 and picked up piano a few years ago. I\'m into rock, pop, and indie music and always looking to expand my skills and knowledge. Let\'s create some awesome music together!',
+},
+{
+  firstName: 'Maria', lastName: 'Garcia', email: 'maria.g@foo.com',
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjRk6Vs47AjyBOAOBNF8Cic74PJNkAcrpUsGDUcCYpdA&usqp=CAU&ec=48600112',
+  instruments: ['Violin'], skillLevel: 'Beginner',
+  genres: ['Classical', 'Folk', 'World'],
+  influences: ['Bach', 'Mozart', 'Yo-Yo Ma', 'Lindsey Stirling', 'Gaelic Storm'],
+  bio: 'I recently started learning violin and am in love with the classical, folk, and world music that can be played with it. Looking for other musicians who can help me learn and grow!',
+},
+{
+  firstName: 'Jacob', lastName: 'Kim', email: 'jacob.kim@bar.com',
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmWpVZGN21gUEgVVUx09wMf8sDsZawEsRqSnerToYRJg&usqp=CAU&ec=48600112',
+  instruments: ['Guitar'], skillLevel: 'Intermediate',
+  genres: ['Rock', 'Blues', 'Funk'],
+  influences: ['Jimi Hendrix', 'Stevie Ray Vaughan', 'Eric Clapton', 'Prince', 'The Red Hot Chili Peppers'],
+  bio: 'I have been playing guitar for several years and am interested in exploring different genres like rock, blues, and funk. I would love to collaborate with other musicians and learn from them.',
+},
+{
+  firstName: 'Sophie', lastName: 'Jones', email: 'sophie.jones@baz.com',
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdeXOLbcQbqTH-1VQt-c_XGEpXCGioogdU_Y6FM8YUbw&usqp=CAU&ec=48600112',
+  instruments: ['Piano'], skillLevel: 'Advanced',
+  genres: ['Classical', 'Jazz', 'Pop'],
+  influences: ['Chopin', 'Bill Evans', 'Elton John', 'Alicia Keys', 'Norah Jones'],
+  bio: 'I have been playing piano for many years and enjoy exploring various genres like classical, jazz, and pop. I am interested in collaborating with other musicians who share my passion for music.',
+},
+{
+  firstName: 'William', lastName: 'Bryant', email: 'william.b@foo.com',
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPNXlBHc6ipu53WmeQZWq6GrLcyiTef4fBXfH5wES-gA&usqp=CAU&ec=48600112',
+  instruments: ['Drums'], skillLevel: 'Intermediate',
+  genres: ['Rock', 'Metal', 'Punk'],
+  influences: ['John Bonham', 'Neil Peart', 'Dave Grohl', 'Travis Barker', 'Ginger Baker'],
+  bio: 'I have been playing drums for a few years and love playing rock, metal, and punk music. I am interested in collaborating with other musicians who share my passion for these genres.',
+},
+{
+  firstName: 'Ava', lastName: 'Chen', email: 'ava.chen@quux.com',
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyPj1d0IG83GglbBjRyl1Ci50m6CQ7QcW7eSKV-A0hAQ&usqp=CAU&ec=48600112',
+  instruments: ['Vocals'], skillLevel: 'Advanced',
+  genres: ['Pop', 'R&B', 'Soul'],
+  influences: ['Whitney Houston', 'Beyonce', 'Adele', 'Sam Cooke', 'Etta James'],
+  bio: 'I am a classically trained singer with experience in pop, R&B, and soul music. I would like to find others with similar musical tastes who would like to get together to see what we can come up with',
+},
+];
+
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const BrowseArtists = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, contacts } = useTracker(() => {
+  const { ready, artists } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Contacts.userPublicationName);
+    // const subscription = Meteor.subscribe(Contacts.userPublicationName);
     // Determine if the subscription is ready
-    const rdy = subscription.ready();
+    // const rdy = subscription.ready();
+    const rdy = true;
     // Get the Contact documents
-    const contactItems = Contacts.collection.find({}).fetch();
+    // const artistItems = dummyArtistData;
     return {
-      contacts: contactItems,
+      artists: dummyArtistData,
       ready: rdy,
     };
   }, []);
 
-  const dummyArtistData = [{
-    firstName: 'Albert', lastName: 'Haynes', email: 'albert.h@foo.com',
-    image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHw%3D&w=1000&q=80',
-    instruments: ['Guitar'], skillLevel: 'intermediate',
-    genres: ['blues', 'psychedellic rock'],
-    influences: ['Hendrix', 'Vaughn', 'B.B. King', 'The Doors', 'Grateful Dead'],
-    bio: 'I have been practicing guitar for a little over a year now.  I love playing gnarly riffs over some heavy bass and looking forward to the jam sessions to come!',
-  },
-  {
-    firstName: 'Samantha', lastName: 'Lee', email: 'samantha.l@foo.com',
-    image: 'https://images.unsplash.com/photo-1564564295391-7f24f26f568b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGt1bHxlbnwwfHwwfHw%3D&w=1000&q=80',
-    instruments: ['Bass Guitar'], skillLevel: 'advanced',
-    genres: ['funk', 'jazz', 'soul'],
-    influences: ['Victor Wooten', 'Marcus Miller', 'Thundercat', 'Earth, Wind & Fire', 'Stevie Wonder'],
-    bio: 'I have been playing bass guitar for 10 years now. I love the rhythm and groove that comes with funk, jazz, and soul music. Looking for fellow musicians who share the same passion and vibe!',
-  },
-  {
-    firstName: 'Alex', lastName: 'Nguyen', email: 'alex.n@foo.com',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQayQiD1x_FnLWOHbMia0mXHg6dyd91gcgFPztytUpnPg&usqp=CAU&ec=48600112',
-    instruments: ['Drums', 'Piano'], skillLevel: 'intermediate',
-    genres: ['rock', 'pop', 'indie'],
-    influences: ['The Beatles', 'Queen', 'Radiohead', 'Coldplay', 'Tame Impala'],
-    bio: 'I started playing drums when I was 12 and picked up piano a few years ago. I\'m into rock, pop, and indie music and always looking to expand my skills and knowledge. Let\'s create some awesome music together!',
-  },
-  {
-    firstName: 'Maria', lastName: 'Garcia', email: 'maria.g@foo.com',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjRk6Vs47AjyBOAOBNF8Cic74PJNkAcrpUsGDUcCYpdA&usqp=CAU&ec=48600112',
-    instruments: ['Violin'], skillLevel: 'beginner',
-    genres: ['classical', 'folk', 'world'],
-    influences: ['Bach', 'Mozart', 'Yo-Yo Ma', 'Lindsey Stirling', 'Gaelic Storm'],
-    bio: 'I recently started learning violin and am in love with the classical, folk, and world music that can be played with it. Looking for other musicians who can help me learn and grow!',
-  },
-  ];
-
   return (ready ? (
     <Container className="py-3">
-      <Row className="justify-content-center">
-        <Col md={7}>
-          <Col className="text-center">
-            <h2>Browse Contacts</h2>
-          </Col>
-
-          <Row xs={3} md={5} lg={7} className="g-4">
-            {contacts.map((contact) => (<Col key={contact._id}><Contact contact={contact} /></Col>))}
-          </Row>
-
-        </Col>
-      </Row>
+      <div className="artist-grid">
+        {artists.map((artist, index) => (
+          <div key={index}>
+            <ArtistCard artistEntry={artist} />
+          </div>
+        ))}
+      </div>
     </Container>
-  ) : <LoadingSpinner />);
+  ) :
+    <LoadingSpinner />
+  );
 };
 
 export default BrowseArtists;
