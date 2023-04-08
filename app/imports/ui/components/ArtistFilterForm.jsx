867bc280658cef9bbed642b065c348709f932cab
Caleb <100178343+CalebMueller-UH@users.noreply.github.com>
2023-04-07 21:33:46 -1000

    Implemented filter, but still improving

---
+++ app/imports/ui/components/ArtistFilterForm.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import '../pages/BrowseArtistsStyle.css';
+
+const instruments = [
+  'Guitar', 'Ukulele', 'Vocals', 'Bass Guitar', 'Drums', 'Piano', 'Violin',
+];
+
+const genres = [
+  'Blues', 'Psychadelic Rock', 'Hawaiian', 'Reggae', 'Funk', 'Jazz', 'Soul',
+  'Rock', 'Pop', 'Indie', 'Classical', 'Folk', 'World', 'Metal', 'Punk', 'R&B',
+];
+
+const skillLevels = ['Beginner', 'Intermediate', 'Advanced'];
+
+const ArtistFilterForm = ({ filter, setFilter }) => {
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFilter({ ...filter, [name]: value });
+  };
+
+  const clearFilters = () => {
+    setFilter({ instrument: '', genre: '', skillLevel: '' });
+  };
+
+  return (
+    <div className="filter-form-container">
+      <Form>
+        <Form.Group controlId="filterInstrument">
+          <Form.Label>Instrument</Form.Label>
+          <Form.Control as="select" name="instrument" value={filter.instrument} onChange={handleInputChange}>
+            <option value="">Any</option>
+            {instruments.map((instrument, index) => (
+              <option key={index} value={instrument}>{instrument}</option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="filterGenre">
+          <Form.Label>Genre</Form.Label>
+          <Form.Control as="select" name="genre" value={filter.genre} onChange={handleInputChange}>
+            <option value="">Any</option>
+            {genres.map((genre, index) => (
+              <option key={index} value={genre}>{genre}</option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="filterSkillLevel">
+          <Form.Label>Skill Level</Form.Label>
+          <Form.Control as="select" name="skillLevel" value={filter.skillLevel} onChange={handleInputChange}>
+            <option value="">Any</option>
+            {skillLevels.map((skillLevel, index) => (
+              <option key={index} value={skillLevel}>{skillLevel}</option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+
+        <div className="d-flex justify-content-center">
+          <Button onClick={clearFilters} className="filterButton">Clear filters</Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+ArtistFilterForm.propTypes = {
+  filter: PropTypes.shape({
+    instruments: PropTypes.string,
+    genres: PropTypes.string,
+    skillLevel: PropTypes.string,
+  }),
+  setFilter: PropTypes.func,
+};
+
+ArtistFilterForm.defaultProps = {
+  filter: {
+    instrument: '',
+    genre: '',
+    skillLevel: '',
+  },
+  setFilter: () => {},
+};
+
+export default ArtistFilterForm;
