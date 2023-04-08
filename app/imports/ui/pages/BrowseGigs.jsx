a8136ed0a5b80f2a5817df0434038c7cf77bee58
Caleb <100178343+CalebMueller-UH@users.noreply.github.com>
2023-04-08 12:08:25 -1000

    Minor changes to gig card css

--- app/imports/ui/pages/BrowseGigs.jsx
+++ app/imports/ui/pages/BrowseGigs.jsx
@@ -15,21 +15,16 @@ const dummyGigs = [{
   skillLevel: 'Advanced',
   genres: ['Bluegrass', 'Folk', 'Americana', 'Country', 'Rockabilly', 'Blues', 'Jazz', 'Gospel', 'World Music', 'Soul', 'Funk', 'Reggae', 'Hip-Hop', 'Electronic', 'Classical', 'Pop', 'Alternative'],
   instruments: ['Fiddle', 'Bass', 'Drums', 'Accordion', 'Banjo', 'Fiddle', 'Bass', 'Drums', 'Accordion', 'Banjo', 'Guitar', 'Mandolin', 'Harmonica', 'Piano', 'Trumpet', 'Saxophone', 'Clarinet', 'Flute', 'Violin', 'Cello'],
-  venue: 'The Summer Festival will take place at The Villa@Hoffman Park, a stunning outdoor venue located in the heart of the city. The Villa is a spacious and elegant property that boasts sprawling gardens, tranquil ponds, and plenty of open space for guests to relax and enjoy the festivities.\n' +
-    '\n' +
-    'As you enter the grounds of The Villa, you\'ll be greeted by the beautiful landscaping and manicured lawns, creating a welcoming and serene atmosphere. The venue is equipped with plenty of seating options, including benches, picnic tables, and lounge areas',
-  about: 'The Summer Festival is an exciting event that takes place every year, showcasing live music, delicious food, and a great atmosphere. This year\'s festival promises to be one of the best yet, with an impressive lineup of talented musicians and mouth-watering food trucks that will keep your taste buds happy all evening long.\n' +
-    '\n' +
-    'The festival is set to take place on Saturday, July 15, from 5:00 pm to 10:00 pm, and will be held at the beautiful Villa@Hoffman Park. This stunning venue is the perfect backdrop for the festival, with its lush green lawns, majestic trees, and tranquil ponds creating an ambiance of relaxation and enjoyment.\n' +
-    '\n' +
-    'The festival is designed to cater to music lovers of all skill levels, with a focus on advanced performers. Whether you\'re a seasoned musician or a beginner, you\'re sure to find something to enjoy at this festival. The genres of music that will be featured at the festival are predominantly Bluegrass, but other genres may also make an appearance.\n' +
-    '\n' +
-    'Instruments that will be showcased during the festival include the fiddle, bass, drums, accordion, and banjo, among others. Whether you\'re a musician yourself or simply a lover of great music, you\'ll be able to appreciate the amazing talent that will be on display.',
+  venue: 'The Summer Festival will take place at The Villa@Hoffman Park, a stunning outdoor venue located in the heart of the city. The Villa is a spacious and elegant property' +
+    'that boasts sprawling gardens, tranquil ponds, and plenty of open space for guests to relax and enjoy the festivities.',
+  about: 'The Summer Festival is an exciting event that takes place every year, showcasing live music, delicious food, and a great atmosphere. This year\'s festival promises' +
+    'to be one of the best yet, with an impressive lineup of talented musicians and mouth-watering food trucks that will keep your taste buds happy all evening long.',
   _id: '1234',
 },
 {
   title: 'Acoustic Night',
-  image: 'https://static.vecteezy.com/system/resources/previews/002/100/548/original/an-acoustic-guitar-cubist-surrealism-painting-modern-abstract-design-a-musical-instrument-abstract-colorful-music-cubism-minimalist-style-guitar-and-music-theme-illustration-vector.jpg',
+  image: 'https://static.vecteezy.com/system/resources/previews/002/100/548/original/an-acoustic-guitar-cubist-surrealism-painting-modern-abstract-design-a-musical-' +
+    'instrument-abstract-colorful-music-cubism-minimalist-style-guitar-and-music-theme-illustration-vector.jpg',
   date: 'Fri. May 12, 7:30 pm to 9:00 pm',
   attendees: ['Samantha', 'Kai', 'Wang'],
   skillLevel: 'Intermediate',
