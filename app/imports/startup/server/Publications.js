f0690163d47cf49186fe9c2370c81530cf4c50d5
Caleb <100178343+CalebMueller-UH@users.noreply.github.com>
2023-04-11 15:00:34 -1000

    Updated Publications.js with the newly added auxiliary collections

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,11 +1,31 @@
 import { Meteor } from 'meteor/meteor';
 import { Artists } from '../../api/artists/Artists';
 import { Gigs } from '../../api/gigs/Gigs';
+import { ArtistsToGenres } from '../../api/artists_to_genres/ArtistsToGenres';
+import { ArtistsToGigs } from '../../api/artists_to_gigs/ArtistsToGigs';
+import { ArtistsToInstruments } from '../../api/artists_to_instruments/ArtistsToInstruments';
+import { InstrumentChoices } from '../../api/instrument_choices/InstrumentChoices';
 
 Meteor.publish(Artists.userPublicationName, function () {
   return Artists.collection.find();
 });
 
+Meteor.publish(ArtistsToGenres.userPublicationName, function () {
+  return ArtistsToGenres.collection.find();
+});
+
+Meteor.publish(ArtistsToGigs.userPublicationName, function () {
+  return ArtistsToGigs.collection.find();
+});
+
+Meteor.publish(ArtistsToInstruments.userPublicationName, function () {
+  return ArtistsToInstruments.collection.find();
+});
+
 Meteor.publish(Gigs.userPublicationName, function () {
   return Gigs.collection.find();
+});
+
+Meteor.publish(InstrumentChoices.userPublicationName, function () {
+  return InstrumentChoices.collection.find();
 });
