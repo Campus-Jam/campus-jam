ec1955450f71514d9f0d2c9c465cfa4289f5af36
Caleb <100178343+CalebMueller-UH@users.noreply.github.com>
2023-04-11 22:43:30 -1000

    Altered CreateJamSession page so that the instruments drop down is populated with the contents of uniqueInstruments

--- app/imports/ui/pages/CreateJamSession.jsx
+++ app/imports/ui/pages/CreateJamSession.jsx
@@ -2,41 +2,56 @@ import React from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import { PageIDs } from '../utilities/ids';
+import { getUniqueInstruments } from './BrowseArtists';
+import { Artists } from '../../api/artists/Artists';
 
-const CreateJamSession = () => (
-  <div id={PageIDs.createJamSessionPage} style={{ backgroundImage: 'url(/images/background.jpg)' }}>
-    <Container>
-      <br />
-      <Card className="py-4">
-        <div className="px-4">
-          <h1 className="text-center"> Create a Jam Session</h1>
-          <Row>
-            <Col>
-              <Form.Group className="mb-3">
-                <Form.Label>Attendees</Form.Label>
-                <Form.Control as="textarea" rows={5} placeholder="Attendees"/>
-                <Form.Label>About</Form.Label>
-                <Form.Control as="textarea" rows={5} placeholder="About"/>
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3">
-                <Form.Label>Venue</Form.Label>
-                <Form.Control placeholder="Venue"/>
-                <Form.Label>Date</Form.Label>
-                <Form.Control placeholder="Date"/>
-                <Form.Label>Genre(s):</Form.Label>
-                <Form.Control placeholder="Venue"/>
-                <Form.Label>Instruments:</Form.Label>
-                <Form.Control placeholder="Instruments"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row><Button>Submit</Button></Row>
-        </div>
-      </Card>
-    </Container>
-  </div>
-);
+const CreateJamSession = () => {
+
+  const artists = Artists.collection.find().fetch();
+  const uniqueInstruments = getUniqueInstruments(artists);
+
+  return (
+    <div id={PageIDs.createJamSessionPage} style={{ backgroundImage: 'url(/images/background.jpg)' }}>
+      <Container>
+        <br />
+        <Card className="py-4">
+          <div className="px-4">
+            <h1 className="text-center"> Create a Jam Session</h1>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>Attendees</Form.Label>
+                  <Form.Control as="textarea" rows={5} placeholder="Attendees" />
+                  <Form.Label>About</Form.Label>
+                  <Form.Control as="textarea" rows={5} placeholder="About" />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>Venue</Form.Label>
+                  <Form.Control placeholder="Venue" />
+                  <Form.Label>Date</Form.Label>
+                  <Form.Control placeholder="Date" />
+                  <Form.Label>Genre(s):</Form.Label>
+                  <Form.Control placeholder="Genre(s)" />
+                  <Form.Label>Instruments:</Form.Label>
+                  <Form.Select>
+                    {uniqueInstruments.map((instrument, index) => (
+                      <option key={index} value={instrument}>
+                        {instrument}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row><Button>Submit</Button></Row>
+          </div>
+        </Card>
+      </Container>
+    </div>
+  );
+
+};
 
 export default CreateJamSession;
