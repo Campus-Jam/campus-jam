298fd25eaa24516adf5044ef7880d44bf180e318
reidlum <reidlum@hawaii.edu>
2023-04-11 20:15:20 -1000

    updated landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Button, Col, Container, Image, Nav, Row } from 'react-bootstrap';
-import { ComponentIDs, PageIDs } from '../utilities/ids';
-import { BrowserRouter as Router, Route, Routes, Navigate, NavLink } from 'react-router-dom';
-
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { PageIDs } from '../utilities/ids';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
@@ -13,14 +11,14 @@ const Landing = () => (
         <Row>
           <Col xs={6}>
             <Container>
-              <h1 style={{ paddingTop: '100px', paddingBottom: '10px', color: 'white', fontSize: '36pt' }}>
+              <h1 style={{ paddingTop: '150px', paddingBottom: '10px', color: 'white', fontSize: '36pt' }}>
                 Ready to Jam With Jamb-UH-ree?
               </h1>
               <h3 style={{ paddingBottom: '70px', color: 'white' }}>
                 Find musicians and plan jam sessions at UH Manoa
               </h3>
               <Button as={NavLink} to="/signup">Create an account and start jamming today!</Button>
-              <p style={{ paddingTop: '5px', paddingBottom: '100px', color: 'white' }}>
+              <p style={{ paddingTop: '5px', paddingBottom: '150px', color: 'white' }}>
                 Already have an account? <NavLink as={NavLink} to="/signin">Log In</NavLink>
               </p>
             </Container>
@@ -32,42 +30,31 @@ const Landing = () => (
       </Container>
     </div>
     <div className="landing-white-background">
-      <Container className="justify-content-center text-center">
-        <h2 style={{ color: '#376551' }}>Start by making your profile....</h2>
-        <Row md={10} lg={2}>
-          <Col xs={10}>
-            <Image src="/images/home-page.png" width={500} />
-          </Col>
-          <Col xs={6}>
-            <Image src="/images/profiles-page.png" width={500} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    <div className="landing-green-background">
-      <Container className="justify-content-center text-center">
-        <h2 style={{ color: 'white' }}>...then add your projects</h2>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/add-project-page.png" width={500} />
-          </Col>
-          <Col xs={6}>
-            <Image src="/images/projects-page.png" width={500} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    <div className="landing-white-background text-center">
-      <h2 style={{ color: '#376551' }}>
-        Connect to people and projects with shared interests!
-      </h2>
       <Container>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/interests-page.png" width={500} />
+        <Row md={10} lg={3}>
+          <Col>
+            <h3 style={{ paddingTop: '50px' }}>
+              Customize your profile
+            </h3>
+            <p style={{ paddingBottom: '50px' }}>
+              Provide information about your favorite genres, what instruments you play, and your skill level so others can discover and learn more about your talents.
+            </p>
           </Col>
-          <Col xs={6}>
-            <Image src="/images/filter-page.png" width={500} />
+          <Col>
+            <h3 style={{ paddingTop: '50px' }}>
+              Browse artists and jam sessions
+            </h3>
+            <p style={{ paddingBottom: '50px' }}>
+              Search through the various artists and jam session at UH Manoa using filters to find the right matches for you.
+            </p>
+          </Col>
+          <Col>
+            <h3 style={{ paddingTop: '50px' }}>
+              Start your own jam sessions
+            </h3>
+            <p style={{ paddingBottom: '50px' }}>
+              Create a jam session with a description, time, date, and place to find others who would like to have a jam session with you.
+            </p>
           </Col>
         </Row>
       </Container>
