be77327e050a2004396edb2f15319e121d134fc7
Caleb <100178343+CalebMueller-UH@users.noreply.github.com>
2023-04-12 17:08:27 -1000

    Updated GigCard.jsx so users can Join and Leave a gig

--- app/imports/ui/components/GigCard.jsx
+++ app/imports/ui/components/GigCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './GigCardStyle.css';
 import { Card, Image, ListGroup, Button } from 'react-bootstrap';
@@ -6,10 +6,9 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { Artists } from '../../api/artists/Artists';
 import { ArtistsToGigs } from '../../api/artistsToGigs/ArtistsToGigs';
-import LoadingSpinner from './LoadingSpinner';
 
 // Maximum Length of Attendees that should be displayed
-// const MAX_CARD_ATTENDEES_LEN = 125;
+const MAX_CARD_ATTENDEES_LEN = 125;
 
 // Maximum Length of Genre(s) that should be displayed
 const MAX_CARD_GENRES_LEN = 125;
@@ -47,10 +46,19 @@ const GigCard = ({ gigEntry }) => {
     const joinedArtists = artists.filter((artist) => joinedArtistIds.includes(artist._id));
     const whosGoing = joinedArtists.map((artist) => artist.firstName);
     return {
-      artistData: currentArtist,
       ready: rdy,
+      artistData: currentArtist,
       attendees: whosGoing };
   });
+
+  const [joined, setJoined] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  useEffect(() => {
+    if (artistData && ArtistsToGigs.collection.find({ artist_id: artistData._id, gig_id: gigEntry._id }).count() > 0) {
+      setJoined(true);
+    }
+  }, [artistData, gigEntry._id]);
 
   const joinGig = () => {
     if (!artistData) {
@@ -58,16 +66,31 @@ const GigCard = ({ gigEntry }) => {
       return;
     }
 
-    Meteor.call('artistsToGigs.insert', artistData._id, gigEntry._id, (error) => {
-      if (error) {
-        alert(`An error occurred while joining the gig: ${error.message}`);
-      } else {
-        alert('Successfully joined the gig!');
-      }
-    });
+    if (!joined) {
+      Meteor.call('artistsToGigs.insert', artistData._id, gigEntry._id, (error) => {
+        if (error) {
+          alert(`An error occurred while joining the gig: ${error.message}`);
+        } else {
+          setJoined('Joined');
+          setButtonDisabled(true);
+          setTimeout(() => {
+            setJoined('Join');
+            setButtonDisabled(false);
+          }, 500);
+        }
+      });
+    } else {
+      Meteor.call('artistsToGigs.remove', artistData._id, gigEntry._id, (error) => {
+        if (error) {
+          alert(`An error occurred while leaving the gig: ${error.message}`);
+        } else {
+          setJoined(false);
+        }
+      });
+    }
   };
 
-  return (ready ? (
+  return ready ? (
     <div className="gigCard">
       <Card className="h-100">
         <Card.Header>
@@ -78,14 +101,15 @@ const GigCard = ({ gigEntry }) => {
             <Image src={gigEntry.image} height={150} className="image-shadow" />
           </div>
         </Card.Header>
-
         <ListGroup variant="flush">
 
+          {/* ATTENDEES */}
           <ListGroup.Item className="d-flex justify-content-between align-items-center attendees">
             <span className="label fw-bold d-flex justify-content-start">Attendees: </span>
-            <span className="content">{attendees.join(', ')}</span>
+            <span className="content">{truncateTo(attendees.join(', '), MAX_CARD_ATTENDEES_LEN)}</span>
           </ListGroup.Item>
 
+          {/* SKILL LEVEL */}
           <ListGroup.Item className="d-flex justify-content-between align-items-center date">
             <span className="label fw-bold d-flex justify-content-start">Date: </span>
             <span className="content">{gigEntry.date}</span>
@@ -93,35 +117,39 @@ const GigCard = ({ gigEntry }) => {
             <span className="content">{gigEntry.skillLevel}</span>
           </ListGroup.Item>
 
+          {/* GENRES */}
           <ListGroup.Item className="d-flex justify-content-between align-items-center genres">
             <span className="label fw-bold d-flex justify-content-start">Genre(s): </span>
             <span className="content">{truncateTo(gigEntry.genres.join(', '), MAX_CARD_GENRES_LEN)}</span>
           </ListGroup.Item>
 
+          {/* INSTRUMENTS */}
           <ListGroup.Item className="d-flex justify-content-between align-items-center instruments">
             <span className="label fw-bold d-flex justify-content-start">Instruments: </span>
             <span className="content">{truncateTo(gigEntry.instruments.join(', '), MAX_CARD_INSTRUMENTS_LEN)}</span>
           </ListGroup.Item>
 
+          {/* VENUE */}
           <ListGroup.Item className="d-flex justify-content-between align-items-center venue">
             <span className="label fw-bold d-flex justify-content-start">Venue: </span>
             <span className="content">{truncateTo(gigEntry.venue, MAX_CARD_VENUE_LEN)}</span>
           </ListGroup.Item>
 
+          {/* ABOUT */}
           <ListGroup.Item className="d-flex justify-content-between align-items-center about">
             <span className="label fw-bold d-flex justify-content-start">About: </span>
             <span className="content">{truncateTo(gigEntry.about, MAX_CARD_ABOUT_LEN)}</span>
             <span className="joinButtonSpan">
-              <Button className="joinButton" onClick={joinGig} disabled={!ready}>Join</Button>
+              <Button className="joinButton" onClick={joinGig} disabled={buttonDisabled}>
+                {joined ? 'Leave' : 'Join'}
+              </Button>
             </span>
           </ListGroup.Item>
 
         </ListGroup>
       </Card>
     </div>
-  ) :
-    <LoadingSpinner />
-  );
+  ) : null;
 };
 
 GigCard.propTypes = {
