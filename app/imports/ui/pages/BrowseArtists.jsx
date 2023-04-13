e7793fc7d9703b7dac7f7150d7c55e051c74198c
Caleb <100178343+CalebMueller-UH@users.noreply.github.com>
2023-04-12 19:41:08 -1000

    Update BrowseArtists.jsx

--- app/imports/ui/pages/BrowseArtists.jsx
+++ app/imports/ui/pages/BrowseArtists.jsx
@@ -10,9 +10,7 @@ import ArtistFilterForm from '../components/ArtistFilterForm';
 import { Artists } from '../../api/artists/Artists';
 
 export const getUniqueInstruments = (artists) => {
-  if (!artists) {
-    return [];
-  }
+  console.log(artists);
   const instruments = new Set();
   artists.forEach((artist) => artist.instruments.forEach((instrument) => instruments.add(instrument)));
   return Array.from(instruments);
