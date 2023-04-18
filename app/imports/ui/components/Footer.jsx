af6851d7bb39ba8370e7fc6bcc2c7f193444712d
jaedench <100117979+jaedench@users.noreply.github.com>
2023-04-18 00:04:51 -1000

    Updated Footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 
 /* The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="footer mt-auto py-3 bg-dark">
-    <Container>
-      <Col className="text-center" style={{ color: 'white' }}>
-        jamb-UH-ree
-        {' '}
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a style={{ color: 'white' }} href="https://jamb-uh-ree.github.io/">https://jamb-uh-ree.github.io/</a>
-      </Col>
+    <Container className="ps-5">
+      <Row>
+        <Col xs={2}>
+          <Container>
+            <Image className="ps-0" src="/images/logo-transparent.png" width={125} />
+            <h5 className="fw-bolder" style={{ color: 'white' }}>jamb-UH-ree</h5>
+          </Container>
+        </Col>
+        <Col xs={3} style={{ color: 'white' }}>
+          <h2 className="pt-2">Visit Us</h2>
+          <hr style={{ border: '3px solid' }} />
+          University of Hawaii
+          <br />
+          Honolulu, HI 96822
+          {' '}
+        </Col>
+        <Col xs={3} style={{ color: 'white' }}>
+          <h2 className="pt-2">Important Links</h2>
+          <hr style={{ border: '3px solid' }} />
+          <a style={{ color: 'white', textDecoration: 'none' }} href="https://jamb-uh-ree.github.io/">Project Home page</a>
+          <br />
+          <a style={{ color: 'white', textDecoration: 'none' }} href="https://docs.google.com/document/d/1RjHlU3JCVSA35spR8NsnANxm8wTX9YFo0xGyXEpNkgY/edit?usp=sharing">Team Contract</a>
+          <br />
+          <a style={{ color: 'white', textDecoration: 'none' }} href="https://github.com/jamb-uh-ree">View On GitHub</a>
+        </Col>
+        <Col xs={3} style={{ color: 'white' }}>
+          <h2 className="pt-2">Meet the Team</h2>
+          <hr style={{ border: '3px solid' }} />
+          <a style={{ color: 'white', textDecoration: 'none' }} href="https://calebmueller-uh.github.io/">Caleb Mueller</a>
+          <br />
+          <a style={{ color: 'white', textDecoration: 'none' }} href="https://elisdiep.github.io/">Elis Diep</a>
+          <br />
+          <a style={{ color: 'white', textDecoration: 'none' }} href="https://jaedench.github.io/">Jaeden Chang</a>
+          <br />
+          <a style={{ color: 'white', textDecoration: 'none' }} href="https://reidlum.github.io/">Reid Lum</a>
+        </Col>
+      </Row>
     </Container>
   </footer>
 );
