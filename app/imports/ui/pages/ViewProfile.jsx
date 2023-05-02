a31cace097de546fd59f4d8e57a61027b42606c8
Caleb <100178343+CalebMueller-UH@users.noreply.github.com>
2023-05-01 18:16:13 -1000

    Update ViewProfile.jsx

--- app/imports/ui/pages/ViewProfile.jsx
+++ app/imports/ui/pages/ViewProfile.jsx
@@ -58,121 +58,120 @@ const ViewProfile = () => {
     setImageSrc(defaultImageSrc);
   };
 
-  if (!ready || !artistToView) {
-    return <LoadingSpinner />;
-  }
-  return (
-    <div id={PageIDs.viewProfilePage} className="viewProfile">
-      <Container className="py-4">
-        <Card id={ComponentIDs.viewProfileForm} className="card">
+  return (!ready || !artistToView) ? (
+    <LoadingSpinner />) :
+    (
+      <div id={PageIDs.viewProfilePage} className="viewProfile">
+        <Container className="py-4">
+          <Card id={ComponentIDs.viewProfileForm} className="card">
 
-          <Card.Title>
-            {/* NAME TITLE AND EMAIL */}
-            <h1 className="text-center nameTitle">
-              {artistToView.firstName} {artistToView.lastName}
-            </h1>
-            <p className="cardText text-center">{id.id}</p>
-          </Card.Title>
+            <Card.Title>
+              {/* NAME TITLE AND EMAIL */}
+              <h1 className="text-center nameTitle">
+                {artistToView.firstName} {artistToView.lastName}
+              </h1>
+              <p className="cardText text-center">{id.id}</p>
+            </Card.Title>
 
-          <Card.Body>
+            <Card.Body>
 
-            {/* IMAGE */}
-            <Row className="d-flex justify-content-center">
-              <Col xs={12} md={4} className="d-flex align-items-center image-col">
-                <Image
-                  src={imageSrc}
-                  height={100}
-                  className="mx-auto d-block img-fluid image"
-                  onError={handleImageError}
-                />
-              </Col>
+              {/* IMAGE */}
+              <Row className="d-flex justify-content-center">
+                <Col xs={12} md={4} className="d-flex align-items-center image-col">
+                  <Image
+                    src={imageSrc}
+                    height={100}
+                    className="mx-auto d-block img-fluid image"
+                    onError={handleImageError}
+                  />
+                </Col>
 
-              {/* INFO */}
-              <Col xs={12} md={8} className="info-col">
+                {/* INFO */}
+                <Col xs={12} md={8} className="info-col">
 
-                {/* INSTRUMENTS & SKILL LEVEL */}
-                <Row>
-                  <Col className="text-start">
-                    <h4 className="cardLabel">Instrument{artistToView.instruments.length > 1 ? 's' : ''} Played</h4>
-                    <p className="cardText">{artistToView.instruments.join(', ')}</p>
-                  </Col>
-                  <Col className="text-start">
-                    <h4 className="cardLabel">Skill-Level</h4>
-                    <p className="cardText">{artistToView.skillLevel}</p>
-                  </Col>
-                </Row>
+                  {/* INSTRUMENTS & SKILL LEVEL */}
+                  <Row>
+                    <Col className="text-start">
+                      <h4 className="cardLabel">Instrument{artistToView.instruments.length > 1 ? 's' : ''} Played</h4>
+                      <p className="cardText">{artistToView.instruments.join(', ')}</p>
+                    </Col>
+                    <Col className="text-start">
+                      <h4 className="cardLabel">Skill-Level</h4>
+                      <p className="cardText">{artistToView.skillLevel}</p>
+                    </Col>
+                  </Row>
 
-                <br />
+                  <br />
 
-                {/* INSTRUMENTS AND INFLUENCES */}
-                <Row>
-                  {/* GENRES */}
-                  <Col className="text-start">
-                    <h4 className="cardLabel">Genres:</h4>
-                    <p className="cardText">{artistToView.genres.join(', ')}</p>
-                  </Col>
+                  {/* INSTRUMENTS AND INFLUENCES */}
+                  <Row>
+                    {/* GENRES */}
+                    <Col className="text-start">
+                      <h4 className="cardLabel">Genres:</h4>
+                      <p className="cardText">{artistToView.genres.join(', ')}</p>
+                    </Col>
 
-                  <Col>
-                    <h4 className="cardLabel">Influences</h4>
-                    <p className="cardText">{artistToView.influences.join(', ')}</p>
-                  </Col>
-                </Row>
+                    <Col>
+                      <h4 className="cardLabel">Influences</h4>
+                      <p className="cardText">{artistToView.influences.join(', ')}</p>
+                    </Col>
+                  </Row>
 
-                <br />
+                  <br />
 
-                {/* BIO */}
-                <Row>
-                  <Col className="text-start">
-                    <h4 className="cardLabel">Bio:</h4>
-                    <p className="cardText">{artistToView.bio}</p>
-                  </Col>
-                </Row>
+                  {/* BIO */}
+                  <Row>
+                    <Col className="text-start">
+                      <h4 className="cardLabel">Bio:</h4>
+                      <p className="cardText">{artistToView.bio}</p>
+                    </Col>
+                  </Row>
 
-              </Col>
-            </Row>
-
-            {/* CREATE JAMSESSION BUTTON */}
-            {currentUser === id.id && (
-              <Row>
-                <Col className="text-end mt-3">
-                  <Button className="editProfileButton">
-                    <Nav.Link
-                      className="EditProfileStyle.css"
-                      as={NavLink}
-                      id={ComponentIDs.createJamSession}
-                      to="/editProfile"
-                      key="editProfile"
-                    >
-                      Edit Profile
-                    </Nav.Link>
-                  </Button>
                 </Col>
               </Row>
+
+              {/* EDIT PROFILE BUTTON */}
+              {currentUser === id.id && (
+                <Row>
+                  <Col className="text-end mt-3">
+                    <Button className="editProfileButton">
+                      <Nav.Link
+                        className="EditProfileStyle.css"
+                        as={NavLink}
+                        id={ComponentIDs.createJamSession}
+                        to="/editProfile"
+                        key="editProfile"
+                      >
+                        Edit Profile
+                      </Nav.Link>
+                    </Button>
+                  </Col>
+                </Row>
+              )}
+
+            </Card.Body>
+          </Card>
+
+          {/* JOINED GIGS */}
+          <div>
+            {gigObj.length > 0 && (
+              <>
+                <h3 className="text-center py-2">
+                  Jam Sessions that {artistToView.firstName} has joined:
+                </h3>
+                <div className="gig-grid">
+                  {gigObj.map((gig) => (
+                    <div key={gig._id}>
+                      <GigCard gigEntry={gig} />
+                    </div>
+                  ))}
+                </div>
+              </>
             )}
+          </div>
 
-          </Card.Body>
-        </Card>
-
-        {/* JOINED GIGS */}
-        <div>
-          {gigObj.length > 0 && (
-            <>
-              <h3 className="text-center py-2">
-                Jam Sessions that {artistToView.firstName} has joined:
-              </h3>
-              <div className="gig-grid">
-                {gigObj.map((gig) => (
-                  <div key={gig._id}>
-                    <GigCard gigEntry={gig} />
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
-        </div>
-
-      </Container>
-    </div>
-  );
+        </Container>
+      </div>
+    );
 };
 export default ViewProfile;
